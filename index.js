2adad0faa56e8728a032d1cc2ec4e34577bd7226
Wangxy <xtwxy@hotmail.com>
2016-09-20 10:04:56 +0800

    add redis connection.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = require("./lib/app");
 
 /* assemble the request handles here to form the application. */
 require("./lib/tree_node");
+require("./lib/slogan");
 
 
 http.createServer(app).listen(8080);
