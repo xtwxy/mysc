fd8e7de1e3568e83c1e07369314796ed2a00bedb
wangdy123 <S_X_wang@163.com>
2016-09-21 16:29:19 +0800

    test transaction

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = require("./lib/app");
 /* assemble the request handles here to form the application. */
 require("./lib/tree_node");
 require("./lib/slogan");
+require("./lib/ctreateNode");
 
 
 http.createServer(app).listen(8080);
