ee7b5329cde1c5d97f54528aa48d498b71b2b942
Wangxy <xtwxy@hotmail.com>
2017-07-13 15:05:54 +0800

    delete unused lines.

--- redis-queue/subscribe/index.js
+++ redis-queue/subscribe/index.js
@@ -1,26 +1,27 @@
 var count = 10000,
-    connections = 10,
+    connections = 300,
     password = "foobared",
     redis = require("redis"),
     client = redis.createClient({
         host: '192.168.0.78',
         port: 6379,
-        //password: password
     });
 
 client.on("error", function(err) {
    console.log("Error: " + err);
 });
 
-for(var i = 0; i < connections; ++i) {
-    var conn = client.duplicate();
+var subscribe = function(conn) {
+
     conn.on("connect", function() {
-        //client.auth(password);
-        //console.log("Authenticated with password: " + password);
         client.subscribe("chat");
     });
 
     conn.on("message", function(channel, message) {
-        //console.log("MSG: " + message);
     });
+};
+
+for(var i = 0; i < connections; ++i) {
+    var conn = client.duplicate();
+    subscribe(conn);
 }
