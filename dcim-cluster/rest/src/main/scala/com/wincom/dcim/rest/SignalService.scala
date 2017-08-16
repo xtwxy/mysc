6c6157a5b9380ecd8788927ea3c7ab80e57e0b35
Wangxy <xtwxy@hotmail.com>
2017-08-16 14:18:15 +0800

    implement Signal marshaling.

--- dcim-cluster/rest/src/main/scala/com/wincom/dcim/rest/SignalService.scala
+++ dcim-cluster/rest/src/main/scala/com/wincom/dcim/rest/SignalService.scala
@@ -1,9 +1,9 @@
 package com.wincom.dcim.rest
 
 import akka.actor.{ActorRef, ActorSystem}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.pattern.ask
 import akka.util.Timeout
 import com.wincom.dcim.domain.Signal.{CreateSignalCmd, SignalVo, _}
@@ -30,12 +30,76 @@ trait SignalRoutes extends SignalMarshaling {
 
   def routes = path("signal" /) {
     post {
-      entity(as[SignalVo]) { s =>
+      entity(as[CreateSignalCmd]) { s =>
         onSuccess(signals.ask(
           CreateSignalCmd(s.signalId, s.name, s.driverId, s.key)).mapTo[Command]) {
           case _ => complete(OK)
         }
       }
-    }
+    } ~
+      path(Segment /) { signalId =>
+        get {
+          complete(OK)
+        } ~
+          put {
+            entity(as[SignalVo]) { s =>
+              complete(OK)
+            }
+          } ~
+          delete {
+            complete(OK)
+          } ~
+        post {
+          path("rename") {
+            pathEnd{
+              entity(as[RenameSignalCmd]) { s =>
+                complete(OK)
+              }
+            }
+          }
+          path("select-driver") {
+            pathEnd {
+              entity(as[SelectDriverCmd]) { s =>
+                complete(OK)
+              }
+            }
+          }
+          path("select-key") {
+            pathEnd {
+              entity(as[SelectKeyCmd]) { s =>
+                complete(OK)
+              }
+            }
+          }
+          path("save-snapshot") {
+            pathEnd {
+              entity(as[SaveSnapshotCmd]) { s =>
+                complete(OK)
+              }
+            }
+          }
+          path("update-value") {
+            pathEnd {
+              entity(as[UpdateValueCmd]) { s =>
+                complete(OK)
+              }
+            }
+          }
+          path("set-value") {
+            pathEnd {
+              entity(as[SetValueCmd]) { s =>
+                complete(OK)
+              }
+            }
+          }
+          path("get-value") {
+            pathEnd {
+              entity(as[GetValueCmd]) { s =>
+                complete(OK)
+              }
+            }
+          }
+        }
+      }
   }
 }
