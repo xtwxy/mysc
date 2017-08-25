c36003199e9044fe358eeaf85e89a65708b839d2
Wangxy <xtwxy@hotmail.com>
2017-08-26 00:29:16 +0800

    add relative dir support.

--- dcim-cluster/project/sum-jar-size.sh
+++ dcim-cluster/project/sum-jar-size.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
-find . -type f -name "*.jar" -exec ls -l {} \; | awk '{ print $5 }' | sh sumline.sh
+PRG_DIR=$(dirname $(readlink -f $0))
+find . -type f -name "*.jar" -exec ls -l {} \; | awk '{ print $5 }' | sh $PRG_DIR/sumline.sh
 
