c8851175c428529d5fc6544a830d6f832e2ad4d2
Wangxy <xtwxy@hotmail.com>
2017-08-31 13:50:02 +0800

    re-implement alarm generator.

--- dcim-cluster/domain/src/main/scala/com/wincom/dcim/domain/Alarm.scala
+++ dcim-cluster/domain/src/main/scala/com/wincom/dcim/domain/Alarm.scala
@@ -22,7 +22,7 @@ import scala.util.Success
   * Created by wangxy on 17-8-28.
   */
 final case class ThresholdFuncVo(name: String, params: Map[String, String])
-final case class AlarmConditionVo(func: ThresholdFuncVo, positiveDesc: String, negativeDesc: String)
+final case class AlarmConditionVo(func: ThresholdFuncVo, level:Int, positiveDesc: String, negativeDesc: String)
 
 object ThresholdFuncVo {
   def apply(name: String, params: Map[String, String]): ThresholdFuncVo = new ThresholdFuncVo(name, params)
@@ -30,8 +30,8 @@ object ThresholdFuncVo {
 }
 
 object AlarmConditionVo {
-  def apply(func: ThresholdFuncVo, positiveDesc: String, negativeDesc: String): AlarmConditionVo = new AlarmConditionVo(func, positiveDesc, negativeDesc)
-  def apply(cond: AlarmCondition): AlarmConditionVo = new AlarmConditionVo(ThresholdFuncVo(cond.func), cond.positiveDesc, cond.negativeDesc)
+  def apply(func: ThresholdFuncVo, level:Int, positiveDesc: String, negativeDesc: String): AlarmConditionVo = new AlarmConditionVo(func, level, positiveDesc, negativeDesc)
+  def apply(cond: AlarmCondition): AlarmConditionVo = new AlarmConditionVo(ThresholdFuncVo(cond.func), cond.level, cond.positiveDesc, cond.negativeDesc)
 }
 
 final case class ThresholdFunc(name: String, params: Map[String, String], func: SetFunction) extends SetFunction {
@@ -40,14 +40,16 @@ final case class ThresholdFunc(name: String, params: Map[String, String], func: 
   override def intersects(f: SetFunction): Boolean = func.intersects(f)
 }
 
-final case class AlarmCondition(func: ThresholdFunc, positiveDesc: String, negativeDesc: String) extends SetFunction {
+final case class AlarmCondition(func: ThresholdFunc, level:Int, positiveDesc: String, negativeDesc: String) extends SetFunction {
   override def contains(e: AnyVal): Boolean = func.contains(e)
   override def subsetOf(f: SetFunction): Boolean = func.subsetOf(f)
   override def intersects(f: SetFunction): Boolean = func.intersects(f)
 }
 
 object Alarm {
-  def props(signalShard: () => ActorRef, registry: FunctionRegistry) = Props(new Alarm(signalShard, registry))
+  def props(signalShard: () => ActorRef,
+            alarmRecordShard: () => ActorRef,
+            registry: FunctionRegistry) = Props(new Alarm(signalShard, alarmRecordShard, registry))
 
   def name(alarmId: String) = s"$alarmId"
 
@@ -58,32 +60,56 @@ object Alarm {
   sealed trait Event extends Serializable
 
   /* value objects */
-
+  final case class AlarmValueVo(alarmId: String,
+                                name: String,
+                                signalId: String,
+                                conditions: Set[Seq[AlarmCondition]],
+                                value: Option[Boolean],
+                                matched: Option[AlarmCondition],
+                                beginTs: Option[DateTime],
+                                endTs: Option[DateTime])
 
   final case class Ok(alarmId: String) extends Command
 
   final case class NotAvailable(alarmId: String) extends Command
 
   /* commands */
+  final case class CreateAlarmCmd(alarmId: String, name: String, signalId: String, conditions: Set[Seq[AlarmConditionVo]]) extends Command
+  final case class RenameAlarmCmd(alarmId: String, newName: String) extends Command
+  final case class SelectSignalCmd(alarmId: String, newSignalId: String) extends Command
+  final case class AddConditionCmd(alarmId: String, condition: AlarmConditionVo) extends Command
+  final case class RemoveConditionCmd(alarmId: String, condition: AlarmConditionVo) extends Command
+  final case class ReplaceConditionCmd(alarmId: String, oldCondition: AlarmConditionVo, newCondition: AlarmConditionVo) extends Command
 
   /* transient commands */
+  final case class RetrieveAlarmCmd(alarmId: String) extends Command
+  final case class EvalAlarmValueCmd(alarmId: String) extends Command
 
+  /* persistent objects */
   /* events */
+  final case class CreateAlarmEvt(name: String, signalId: String, conditions: Set[Seq[AlarmConditionVo]]) extends Event
+  final case class RenameAlarmEvt(newName: String) extends Event
+  final case class SelectSignalEvt(newSignalId: String) extends Event
+  final case class AddConditionEvt(condition: AlarmConditionVo) extends Event
+  final case class RemoveConditionEvt(condition: AlarmConditionVo) extends Event
+  final case class ReplaceConditionEvt(oldCondition: AlarmConditionVo, newCondition: AlarmConditionVo) extends Event
 
+  /* snapshot */
+  final case class AlarmPo(alarmId: String, name: String, signalId: String, conditions: Set[Seq[AlarmConditionVo]])
 }
 
-class Alarm(signalShard: () => ActorRef, registry: FunctionRegistry) extends PersistentActor {
+class Alarm(signalShard: () => ActorRef,
+            alarmRecordShard: () => ActorRef,
+            registry: FunctionRegistry) extends PersistentActor {
   val log = Logging(context.system.eventStream, "sharded-alarms")
 
   val alarmId = s"${self.path.name}"
   var alarmName: Option[String] = None
-  var alarmLevel: Option[Int] = None
   var signalId: Option[String] = None
 
   // transient values
   var conditions: Set[Seq[AlarmCondition]] = Set()
   var value: Option[Boolean] = None
-  var desc: Option[String] = None
   var matched: Option[AlarmCondition] = None
   var beginTs: Option[DateTime] = None
   var endTs: Option[DateTime] = None
@@ -95,7 +121,7 @@ class Alarm(signalShard: () => ActorRef, registry: FunctionRegistry) extends Per
     context.system.scheduler.schedule(0 milliseconds,
       evalPeriod,
       self,
-      alarmId
+      EvalAlarmValueCmd(alarmId)
     )
   }
 
@@ -110,6 +136,22 @@ class Alarm(signalShard: () => ActorRef, registry: FunctionRegistry) extends Per
   }
 
   override def receiveCommand: Receive = {
+    case CreateAlarmCmd(_, name, signalId, conds) =>
+      persist(CreateAlarmEvt(name, signalId, conds))(updateState)
+    case SelectSignalCmd(_, newSignalId) =>
+      persist(SelectSignalEvt(newSignalId))(updateState)
+    case AddConditionCmd(_, condition) =>
+      persist(AddConditionEvt(condition))(updateState)
+    case RemoveConditionCmd(_, condition) =>
+      persist(RemoveConditionEvt(condition))(updateState)
+    case ReplaceConditionCmd(_, old, newOne) =>
+      persist(ReplaceConditionEvt(old, newOne))(updateState)
+    case EvalAlarmValueCmd(_) =>
+      signalShard() ! Signal.GetValueCmd(signalId.get)
+    case sv: Signal.SignalValueVo =>
+      evalConditionsWith(sv)
+    case RetrieveAlarmCmd(_) =>
+      sender() ! AlarmValueVo(alarmId, alarmName.get, signalId.get, conditions, value, matched, beginTs, endTs)
     case x => log.info("COMMAND *IGNORED*: {} {}", this, x)
   }
 
@@ -117,24 +159,35 @@ class Alarm(signalShard: () => ActorRef, registry: FunctionRegistry) extends Per
     case x => log.info("EVENT *IGNORED*: {} {}", this, x)
   }
 
-  private def evalConditions(sv: SignalValueVo): Unit = {
+  private def evalConditionsWith(sv: SignalValueVo): Unit = {
     for(a <- conditions) {
       for(c <- a) {
         if(c.contains(sv.value)) {
           if(value.isDefined && value.get) {
             if(c.eq(matched.get)) {
-              // no changes.
+              // no changes. continue alarming state, no transition and no ending.
             } else {
               // transition.
+              matched = Some(c)
+              alarmRecordShard() ! AlarmRecord.TransitAlarmCmd(alarmId, beginTs.get, sv.ts, c.level, sv, c.positiveDesc)
             }
           } else {
-              // new alarm.
+            // new alarm.
+            value = Some(true)
+            matched = Some(c)
+            beginTs = Some(sv.ts)
+            alarmRecordShard() ! AlarmRecord.RaiseAlarmCmd(alarmId, beginTs.get, alarmName.get, c.level, sv, c.positiveDesc)
           }
+          // break the iterations. only the first match matters.
           return
         }
       }
     }
-    // None matched.
-    return
+    // None matched. no alarm or, end of alarm if current state is in alarming state.
+    if(value.isDefined && value.get) {
+      value = Some(false)
+      endTs = Some(sv.ts)
+      alarmRecordShard() ! AlarmRecord.EndAlarmCmd(alarmId, beginTs.get, endTs.get, sv, matched.get.negativeDesc)
+    }
   }
 }
