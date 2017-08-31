656dce98232bde2527099593b76270a8bbc9f6e0
Wangxy <xtwxy@hotmail.com>
2017-08-31 18:18:18 +0800

    implement devide of conditions.

--- dcim-cluster/domain/src/main/scala/com/wincom/dcim/domain/Alarm.scala
+++ dcim-cluster/domain/src/main/scala/com/wincom/dcim/domain/Alarm.scala
@@ -3,47 +3,116 @@ package com.wincom.dcim.domain
 import akka.actor.{ActorRef, Props}
 import akka.event.Logging
 import akka.http.scaladsl.model.DateTime
-import akka.pattern.ask
-import akka.persistence.{PersistentActor, SnapshotOffer}
+import akka.persistence.PersistentActor
 import akka.util.Timeout
 import com.wincom.dcim.domain.Alarm._
 import com.wincom.dcim.domain.Signal.SignalValueVo
-import com.wincom.dcim.signal.{FunctionRegistry, SetFunction, UnaryFunction}
-import org.joda.time.Duration
+import com.wincom.dcim.signal.{FunctionRegistry, SetFunction}
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.language.postfixOps
-import scala.util.Success
 
 /**
   * Created by wangxy on 17-8-28.
   */
 final case class ThresholdFuncVo(name: String, params: Map[String, String])
-final case class AlarmConditionVo(func: ThresholdFuncVo, level:Int, positiveDesc: String, negativeDesc: String)
+
+final case class AlarmConditionVo(func: ThresholdFuncVo, level: Int, positiveDesc: String, negativeDesc: String)
 
 object ThresholdFuncVo {
   def apply(name: String, params: Map[String, String]): ThresholdFuncVo = new ThresholdFuncVo(name, params)
+
   def apply(func: ThresholdFunc): ThresholdFuncVo = new ThresholdFuncVo(func.name, func.params)
 }
 
 object AlarmConditionVo {
-  def apply(func: ThresholdFuncVo, level:Int, positiveDesc: String, negativeDesc: String): AlarmConditionVo = new AlarmConditionVo(func, level, positiveDesc, negativeDesc)
+  def apply(func: ThresholdFuncVo, level: Int, positiveDesc: String, negativeDesc: String): AlarmConditionVo = new AlarmConditionVo(func, level, positiveDesc, negativeDesc)
+
   def apply(cond: AlarmCondition): AlarmConditionVo = new AlarmConditionVo(ThresholdFuncVo(cond.func), cond.level, cond.positiveDesc, cond.negativeDesc)
 }
 
-final case class ThresholdFunc(name: String, params: Map[String, String], func: SetFunction) extends SetFunction {
+final class ThresholdFunc(val name: String, val params: Map[String, String], val func: SetFunction) extends SetFunction {
   override def contains(e: AnyVal): Boolean = func.contains(e)
+
   override def subsetOf(f: SetFunction): Boolean = func.subsetOf(f)
+
   override def intersects(f: SetFunction): Boolean = func.intersects(f)
+
+  override def equals(other: Any): Boolean = other match {
+    case that: ThresholdFunc =>
+      name == that.name &&
+        params == that.params
+    case _ => {
+      println("false: " + other)
+      false
+    }
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(name, params)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
 }
 
-final case class AlarmCondition(func: ThresholdFunc, level:Int, positiveDesc: String, negativeDesc: String) extends SetFunction {
+final class AlarmCondition(val func: ThresholdFunc, val level: Int, val positiveDesc: String, val negativeDesc: String) extends SetFunction {
   override def contains(e: AnyVal): Boolean = func.contains(e)
-  override def subsetOf(f: SetFunction): Boolean = func.subsetOf(f)
-  override def intersects(f: SetFunction): Boolean = func.intersects(f)
+
+  override def subsetOf(f: SetFunction): Boolean = {
+    f match {
+      case t: AlarmCondition =>
+        func.subsetOf(t.func.func)
+      case _ =>
+        func.subsetOf(f)
+    }
+  }
+
+  override def intersects(f: SetFunction): Boolean = {
+    f match {
+      case t: AlarmCondition =>
+        func.intersects(t.func.func)
+      case _ =>
+        func.subsetOf(f)
+    }
+  }
+
+  override def equals(other: Any): Boolean = other match {
+    case that: AlarmCondition =>
+      func == that.func &&
+        level == that.level &&
+        positiveDesc == that.positiveDesc &&
+        negativeDesc == that.negativeDesc
+    case _ => {
+      println("false: " + other)
+      false
+    }
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(func, level, positiveDesc, negativeDesc)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
+}
+
+object ThresholdFunc {
+  def apply(name: String, params: Map[String, String], func: SetFunction): ThresholdFunc = new ThresholdFunc(name, params, func)
+
+  def apply(f: ThresholdFuncVo)(implicit registry: FunctionRegistry): ThresholdFunc = {
+    val func = registry.createUnary(f.name, f.params.asJava)
+    if (func.isDefined) {
+      new ThresholdFunc(f.name, f.params, func.get.asInstanceOf[SetFunction])
+    } else {
+      throw new IllegalArgumentException(f.toString)
+    }
+  }
+}
+
+object AlarmCondition {
+  def apply(func: ThresholdFunc, level: Int, positiveDesc: String, negativeDesc: String): AlarmCondition = new AlarmCondition(func, level, positiveDesc, negativeDesc)
+
+  def apply(c: AlarmConditionVo)(implicit registry: FunctionRegistry): AlarmCondition = new AlarmCondition(ThresholdFunc(c.func), c.level, c.positiveDesc, c.negativeDesc)
 }
 
 object Alarm {
@@ -75,32 +144,44 @@ object Alarm {
 
   /* commands */
   final case class CreateAlarmCmd(alarmId: String, name: String, signalId: String, conditions: Set[Seq[AlarmConditionVo]]) extends Command
+
   final case class RenameAlarmCmd(alarmId: String, newName: String) extends Command
+
   final case class SelectSignalCmd(alarmId: String, newSignalId: String) extends Command
+
   final case class AddConditionCmd(alarmId: String, condition: AlarmConditionVo) extends Command
+
   final case class RemoveConditionCmd(alarmId: String, condition: AlarmConditionVo) extends Command
+
   final case class ReplaceConditionCmd(alarmId: String, oldCondition: AlarmConditionVo, newCondition: AlarmConditionVo) extends Command
 
   /* transient commands */
   final case class RetrieveAlarmCmd(alarmId: String) extends Command
+
   final case class EvalAlarmValueCmd(alarmId: String) extends Command
 
   /* persistent objects */
   /* events */
   final case class CreateAlarmEvt(name: String, signalId: String, conditions: Set[Seq[AlarmConditionVo]]) extends Event
+
   final case class RenameAlarmEvt(newName: String) extends Event
+
   final case class SelectSignalEvt(newSignalId: String) extends Event
+
   final case class AddConditionEvt(condition: AlarmConditionVo) extends Event
+
   final case class RemoveConditionEvt(condition: AlarmConditionVo) extends Event
-  final case class ReplaceConditionEvt(oldCondition: AlarmConditionVo, newCondition: AlarmConditionVo) extends Event
+
+  final case class ReplaceConditionEvt(old: AlarmConditionVo, newOne: AlarmConditionVo) extends Event
 
   /* snapshot */
   final case class AlarmPo(alarmId: String, name: String, signalId: String, conditions: Set[Seq[AlarmConditionVo]])
+
 }
 
 class Alarm(signalShard: () => ActorRef,
             alarmRecordShard: () => ActorRef,
-            registry: FunctionRegistry) extends PersistentActor {
+            implicit val registry: FunctionRegistry) extends PersistentActor {
   val log = Logging(context.system.eventStream, "sharded-alarms")
 
   val alarmId = s"${self.path.name}"
@@ -126,8 +207,9 @@ class Alarm(signalShard: () => ActorRef,
   }
 
   implicit def requestTimeout: Timeout = FiniteDuration(20, SECONDS)
+
   implicit def executionContext: ExecutionContext = context.dispatcher
-  
+
   override def persistenceId: String = s"${self.path.name}"
 
   override def receiveRecover: Receive = {
@@ -156,15 +238,27 @@ class Alarm(signalShard: () => ActorRef,
   }
 
   private def updateState: (Event => Unit) = {
+    case CreateAlarmEvt(name, signalId, conds) =>
+      this.alarmName = Some(name)
+      this.signalId = Some(signalId)
+      createConditions(conds)
+    case SelectSignalEvt(newSignalId) =>
+      this.signalId = Some(newSignalId)
+    case AddConditionEvt(condition) =>
+      addCondition(condition)
+    case RemoveConditionEvt(condition) =>
+      removeCondition(condition)
+    case ReplaceConditionEvt(old, newOne) =>
+      replaceCondition(old, newOne)
     case x => log.info("EVENT *IGNORED*: {} {}", this, x)
   }
 
   private def evalConditionsWith(sv: SignalValueVo): Unit = {
-    for(a <- conditions) {
-      for(c <- a) {
-        if(c.contains(sv.value)) {
-          if(value.isDefined && value.get) {
-            if(c.eq(matched.get)) {
+    for (a <- conditions) {
+      for (c <- a) {
+        if (c.contains(sv.value)) {
+          if (value.isDefined && value.get) {
+            if (c.eq(matched.get)) {
               // no changes. continue alarming state, no transition and no ending.
             } else {
               // transition.
@@ -184,10 +278,61 @@ class Alarm(signalShard: () => ActorRef,
       }
     }
     // None matched. no alarm or, end of alarm if current state is in alarming state.
-    if(value.isDefined && value.get) {
+    if (value.isDefined && value.get) {
       value = Some(false)
       endTs = Some(sv.ts)
       alarmRecordShard() ! AlarmRecord.EndAlarmCmd(alarmId, beginTs.get, endTs.get, sv, matched.get.negativeDesc)
     }
+  }
+
+  private def createConditions(conds: Set[Seq[AlarmConditionVo]]): Unit = {
+    for (cs <- conds) {
+      var seq: mutable.Seq[AlarmCondition] = mutable.ArraySeq()
+      for (c <- cs) {
+        seq = seq :+ AlarmCondition(c)
+      }
+      if (!seq.isEmpty) this.conditions = this.conditions + seq
+    }
+  }
+
+  private def addCondition(cond: AlarmConditionVo): Unit = {
+    var set: mutable.Set[AlarmCondition] = mutable.Set()
+    set = set + AlarmCondition(cond)
+    for (cs <- conditions) {
+      for (c <- cs) {
+        set = set + c
+      }
+    }
+    var seq: mutable.Seq[AlarmCondition] = mutable.Seq()
+    for(c <- set) seq = seq :+ c
+    for(i <- 0 to seq.size) {
+      for(j <- 1 to seq.size) {
+        if(seq(i).subsetOf(seq(j))) {
+
+        } else if(seq(j).subsetOf(seq(i))) {
+
+        } else {
+
+        }
+      }
+    }
+  }
+
+  private def removeCondition(cond: AlarmConditionVo): Unit = {
+    var newConditions: Set[Seq[AlarmCondition]] = Set()
+    for (cs <- conditions) {
+      var seq: mutable.Seq[AlarmCondition] = mutable.ArraySeq()
+      for (c <- cs) {
+        if (!cond.equals(AlarmConditionVo(c))) {
+          seq = seq :+ c
+        }
+      }
+      if (!seq.isEmpty) newConditions = newConditions + seq
+    }
+  }
+
+  private def replaceCondition(old: AlarmConditionVo, newOne: AlarmConditionVo): Unit = {
+    removeCondition(old)
+    addCondition(newOne)
   }
 }
