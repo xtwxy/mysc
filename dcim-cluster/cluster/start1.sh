c1cea4cd7514bdb966e4d169c23f162c3a63589e
master <master@localhost.localdomain>
2017-09-18 11:20:14 +0800

    modify start ip address.

--- dcim-cluster/cluster/start1.sh
+++ dcim-cluster/cluster/start1.sh
@@ -2,5 +2,5 @@
 
 PRG_DIR=$(dirname $(readlink -f $0))
 cd $PRG_DIR
-java -DHOSTNAME=192.168.0.78 -DHOST=192.168.0.78 -DPORT=2553 -DHTTPPORT=8080 -jar target/scala-2.12/cluster.jar
+java -DHOSTNAME=192.168.0.165 -DHOST=192.168.0.165 -DPORT=2553 -DHTTPPORT=8080 -jar target/scala-2.12/cluster.jar
 
